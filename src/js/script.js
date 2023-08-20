f7421532540f47405749bc6f1f0b455e97dc0537
sebbyapudo <sebbyapudo@gmail.com>
2023-08-21 02:35:22 +0300

    Implemented the preloading on load events

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-// Code goes here
+'use strict'
+
+// Add Event on multiple elements
+
+const addEventOnElements = (elements, eventType, callback) => {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+// PRELOADING 
+
+const loadingElement = document.querySelector("[data-loading]");
+
+window.addEventListener("load", () => {
+  loadingElement.classList.add("loaded")
+})
