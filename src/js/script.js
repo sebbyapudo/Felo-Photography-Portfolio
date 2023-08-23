3a8d562207276d7d1c7e9cd8221dbe373ac481aa
sebbyapudo <sebbyapudo@gmail.com>
2023-08-23 22:54:59 +0300

    Implemented the backToTop scroll btn on scroll events

--- src/js/script.js
+++ src/js/script.js
@@ -139,3 +139,26 @@ const setLetterEffect = function () {
 // call the letter effect function after window loaded
 window.addEventListener("load", setLetterEffect);
 
+
+/**
+ * BACK TO TOP BUTTON
+ */
+
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  const bodyHeight = document.body.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const scrollEndPos = bodyHeight - windowHeight;
+  const totalScrollPercent = (window.scrollY / scrollEndPos) * 100;
+
+  backTopBtn.textContent = `${totalScrollPercent.toFixed(0)}%`;
+
+  // visible back top btn when scrolled 5% of the page
+  if (totalScrollPercent > 5) {
+    backTopBtn.classList.add("show");
+  } else {
+    backTopBtn.classList.remove("show");
+  }
+});
+
